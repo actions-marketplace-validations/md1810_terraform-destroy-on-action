77e466dca42f31da4f7e6cddb4dc4261e756de43
Mayur Duduka <71468349+md1810@users.noreply.github.com>
2022-01-28 13:06:14 +0530

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-git clone https://github.com/INPUT_ORG_NAME/$INPUT_REPO_NAME
+git clone https://$TOKEN_GITHUB@github.com/$INPUT_ORG_NAME/$INPUT_REPO_NAME
 cd $INPUT_REPO_NAME/terraform
 terraform init && terraform state replace-provider registry.terraform.io/-/aws registry.terraform.io/hashicorp/aws -auto-approve
 terraform init && terraform destroy -auto-approve
